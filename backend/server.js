75488afea6582a84e15ad2473eb92e99f2ed8f8c
alphaDevloper <abdullahdanyal1234@gmail.com>
2026-01-19 17:11:31 +0500

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+// console.log(process.env.PORT);
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
